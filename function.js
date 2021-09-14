907b731430145fb512508f5405c858ecc6ea4c26
cos <72692386+junzerokim@users.noreply.github.com>
2021-09-15 00:10:02 +0900

    study

--- function.js
+++ function.js
@@ -18,3 +18,47 @@ function log(message) {
   console.log(message);
 }
 log('Hello World')
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+  obj.name = 'coder';
+}
+const cos = { name: 'cos' };
+changeName(cos);
+console.log(cos);
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+  console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+  
+  for (const arg of args) {
+    console.log(arg)
+  }
+
+  args.forEach((arg) => console.log(arg))
+}
+printAll('dream', 'coding', 'cos');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+  let message = 'hello';
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let childMessage = 'hello';
+  }
+  // console.log(childMessage); define error
+}
+printMessage();
