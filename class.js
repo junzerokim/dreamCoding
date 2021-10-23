7e62faea0c7fc68d3a165908281d7dbd0e59d0ab
cos <72692386+junzerokim@users.noreply.github.com>
2021-10-23 22:58:27 +0900

    TIL

--- class.js
+++ class.js
@@ -1,91 +1,103 @@
-// 'use strict';
-// // Object-oriented programming
-// // class: template
-// // object: instance of a class
-// // JavaScript classes
-// // - introduced in ES6
-// // - syntactical sugar over prototype-based inheritance
+'use strict';
+// Object-oriented programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
 
-// // 1. Class declarations
-// class Person {
-//   // constructor
-//   constructor(name, age) {
-//     // fields
-//     this.name = name;
-//     this.age = age;
-//   }
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
 
-//   // methods
-//   speak() {
-//     console.log(`${this.name}: hello!`)
-//   }
-// }
-
-// const cos = new Person('cos', 20);
-// console.log(cos.name);
-// console.log(cos.age);
-// cos.speak();
-
-// // 2. Getter and setters
-// class User {
-//   constructor(firstName, lastName, age) { 
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.age = age;
-//   }
-
-//   get age() {
-//     return this._age;
-//   }
-
-//   set age(value) {
-//     // if (value < 0) {
-//     //   throw Error('age can not be negative')
-//     // }
-//     this._age = value < 0 ? 0 : value;
-//   }
-// }
-
-// const user1 = new User('Steve', 'Job', -1);
-// console.log(user1.age);
-
-// // 3. Fields (public, private)
-// // Too soon!
-
-// class Experiment {
-//   publicField = 2;
-//   #privateField = 0;
-// }
-// const experiment = new Experiment();
-// console.log(experiment.publicField);
-// console.log(experiment.privateField);
-
-// class Counter {
-//   constructor() {
-//     this.counter = 0;
-//   }
-
-//   increase(runIf5Times) {
-//     this.counter++;
-//     console.log(this.counter);
-//     if(this.counter % 5 === 0) {
-//       runIf5Times();
-//     }
-//   }
-// }
-
-let cosmos = {
-  name: 'cos',
-  age: '29',
-  getName: function() {
-    return this.name;
-  },
-  getAge: function() {
-    return this.age;
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`)
   }
 }
 
-const cosmosName = cosmos.getName();
-console.log(cosmosName);
-console.log('hello');
-console.log('hi');
+const cos = new Person('cos', 20);
+console.log(cos.name);
+console.log(cos.age);
+cos.speak();
+
+// 2. Getter and setters
+class User {
+  constructor(firstName, lastName, age) { 
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    // if (value < 0) {
+    //   throw Error('age can not be negative')
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
+
+// 3. Fields (public, private)
+// Too soon!
+
+class Experiment {
+  publicField = 2;
+  #privateField = 0; //클래스 내부에서만 볼 수 있고 외부에서는 볼 수 없다.
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+
+class Article {
+  static publisher = 'Dream Coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
+
+// 5. Inheritance
+// A way for one class to extend another class.
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+  
+  draw() {
+    console.log(`drawing ${this.color} color of`);
+  }
+
+  getArea() {
+    return width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
