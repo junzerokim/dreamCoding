78b61b05136aac8d4ca54a2d5d01345ad0bbc138
cos <junzerokim@gmail.com>
2021-12-05 22:56:33 +0900

    TIL

--- array-api.js
+++ array-api.js
@@ -48,11 +48,18 @@ const students = [
 // Q5. find a student with the score 90
 {
   const result = students.find((student) => student.score === 90);
-  // console.log(result)
+  console.log(result)
 }
 
 // Q6. make an array of enrolled students
 {
   const result = students.filter((student) => student.enrolled);
+  console.log(result);
+}
+
+// Q7. make an array containing only the student's scores
+// result should be: [45, 80, 90, 66, 88]
+{
+  const result = students.map((student) => student.score);
   console.log(result);
 }
